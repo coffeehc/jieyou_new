a19c2bdf474a5ce9308cb9014efd915d1db3a7a5
unknown <hc_my5ill@163.com>
2017-08-24 14:43:03 +0800

    修改游戏

--- application/admin/controller/Game.php
+++ application/admin/controller/Game.php
@@ -30,8 +30,11 @@ class Game extends BaseController {
             if(!$validate->scene('add')->check($data)) {
                 return show(0,$validate->getError());
             }
+            // 获取游戏名字首字母 并大写
             $data['zm'] = strtoupper(substr($data['gid'],0,1));
+            // 获取平台名字
             $data['pt'] = getGamePtNameByPtid($data['ptid']);
+            // 游戏类型名字
             $data['lx'] = getGametypeNameByLxid($data['lxid']);
             try {
                 $res = $this->_db->save($data);
@@ -49,6 +52,47 @@ class Game extends BaseController {
             return $this->fetch('',[
                 'gamePt' => $gamePt,
                 'gameClass' => $gameClass,
+            ]);
+        }
+    }
+
+    /**
+     * 修改游戏
+     * @param  integer $id [description]
+     * @return [type]      [description]
+     */
+    public function edit($id=0) {
+        if(request()->isPost()) {
+            $data = input('post.');
+            $validate = validate('Game');
+            if(!$validate->scene('add')->check($data)) {
+                return show(0,$validate->getError());
+            }
+            // 获取游戏名字首字母 并大写
+            $data['zm'] = strtoupper(substr($data['gid'],0,1));
+            // 获取平台名字
+            $data['pt'] = getGamePtNameByPtid($data['ptid']);
+            // 游戏类型名字
+            $data['lx'] = getGametypeNameByLxid($data['lxid']);
+            try {
+                $res = $this->_db->save($data,['id'=>$data['id']]);
+                if($res) {
+                    return show(1,'修改成功');
+                }else {
+                    return show(0,'修改失败');
+                }
+            } catch (\Exception $e) {
+                return show(0,$e->getMessage());
+            }
+
+        }else {
+            $game = $this->_db->get($id);
+            $gamePt = model('Apikey')->getGamePt();
+            $gameClass = model('GameType')->getGameTypeInfo();
+            return $this->fetch('',[
+                'gamePt' => $gamePt,
+                'gameClass' => $gameClass,
+                'game' => $game,
             ]);
         }
     }
