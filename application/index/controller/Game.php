380445f3053b0f69909a7554d7290b98f3002080
unknown <hc_my5ill@163.com>
2017-09-04 16:20:53 +0800

    对接游戏api

--- application/index/controller/Game.php
+++ application/index/controller/Game.php
@@ -48,8 +48,67 @@ class Game extends BaseController {
                 }
             }
         }else {
-            
-            return $this->fetch();
+            // 登录的用户信息
+            $user = $this->getLoginUser();
+            $serverId = input('param.id');
+            // 是否有传入游戏编号
+            if(empty($serverId)) {
+                // 如果没有服务器ID 则获取最近开服的服务器信息
+                $serverInfo = model('GameServer')->field(true)->where('create_time < '.time())->order('id desc')->find();
+            }else {
+                // 如果有服务器编号 则通过游戏编号获取服务器信息
+                $serverInfo = model('GameServer')->field(true)->where("id = $serverId")->find();
+            }
+            // 根据游戏gid 获取游戏信息
+            $gameGid = $serverInfo['gid'];
+            $gameInfo = model('Game')->field(true)->where("gid = '$gameGid'")->find();
+
+            if($user && $user['id']) {
+                $userInfo = model('User')->field(true)->where('id = '.$user['id'])->find();
+                // 如果游戏开区时间大于当前时间 则提示游戏还没有开区
+                if($serverInfo['create_time'] > time()) {
+                    return $this->error('游戏还没有开启');
+                }else {
+                    // 如果游戏已经开区 则添加一条数据到数据库
+                    $isUserServer = model('UserServer')->field(true)->where("gsid = ".$serverInfo['id']." and userid = ".$userInfo['id'])->find();
+                    if($isUserServer) {
+                        model('UserServer')->where("userid = ".$userInfo['id']." and gsid = ".$serverInfo['id'])->update();
+                    }else {
+                        $userServer = [
+                            'name' => $serverInfo['game'].$serverInfo['name'],
+                            'gsid' => $serverInfo['id'],
+                            'userid' => $userInfo['id']
+                        ];
+                        model('UserServer')->save($userServer);
+                    }
+                }
+
+                // 对接游戏借口
+                $apijk = model('Apikey')->field(true)->where('id = '.$serverInfo['ptid'])->find();
+
+                // 构造接口数据
+                $uid = trim($userInfo['id']);
+                $uid2 = $userInfo['users'];
+                $gid = $serverInfo['gid'];
+                $sid = $serverInfo['sid'];
+                $dateline = time();
+                $mid = $apijk['pid'];
+                $pc = '0';
+                $fcm = '0';
+                $key = $apijk['apikey'];
+
+                if($serverInfo['ptid'] == 2) {
+                    $sign = strtolower(md5($mid.$gid.$sid.$uid2.$dateline.$key));
+                    if(!empty($_GET['client']) && $_GET['client'] == 'pc') {
+                        $url = "http://www.ufojoy.com/auth/go.phtml?agent_id=".$mid."&game_id=".$gid."&serverNo=".$sid."&uid=".$uid2."&time=".$dateline."&sign=".$sign."&client=pc";
+                    }else {
+                        $url = "http://www.ufojoy.com/auth/go.phtml?agent_id=".$mid."&game_id=".$gid."&serverNo=".$sid."&uid=".$uid2."&time=".$dateline."&sign=".$sign;
+                    }
+                }
+            }
+            return $this->fetch('',[
+                'url' => $url,
+            ]);
         }
     }
 }
