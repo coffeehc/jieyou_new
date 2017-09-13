41c33e666f69a3b01a324a1f7cc9ad09e5cb5356
unknown <hc_my5ill@163.com>
2017-09-13 18:15:06 +0800

    支付API curl 换成 from提交

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -21,4 +21,100 @@ class Pay extends User {
             'payWays' => $payWays,
         ]);
     }
+
+    public function ispay() {
+        if(!request()->isPost()) {
+            return $this->error('请求错误');
+        }
+        $data = input('post.');
+        // 如果提交了充值信息
+        if($data['forms'] == 'pay') {
+            // 获取充值的游戏信息
+            $gameServerInfo = model('GameServer')->getGameServerByid($data['sid']);
+            if(!$gameServerInfo) {
+                return $this->error('系统错误');
+            }
+
+            // 获取游戏对应的代理商平台接口信息
+            $ptInfo = model('Apikey')->getPtInfoById($gameServerInfo['ptid']);
+            if(!$ptInfo) {
+                return $this->error('系统错误');
+            }
+            // 获取登录的用户信息
+            $user = session('user','','index');
+            if(!$user) {
+                return $this->error('登录超时，请重新登录');
+            }
+
+            // 获取支付方式 信息
+            $payInfo = model('Payfs')->getPayfsInfoById($data['bank']);
+
+            $key = $ptInfo['paykey'];
+            if($gameServerInfo['ptid'] == 1) {
+                $oid = date('Ymdhis',time()).rand(1000,9999);
+                $uid = $user['id'];
+                $gid = $gameServerInfo['gid'];
+                $sid = $gameServerInfo['sid'];
+                $money = intval($data['money']);
+                $mid = $ptInfo['pid'];
+                $pid = $payInfo['type'];
+                $bank = $payInfo['pay'];
+                $sign = md5($oid.$uid.$gid.$sid.$mid.$money.$pid.$bank.$key);
+                // 添加一条数据到数据库
+                $insertData = [
+                    'oid' => $oid,
+                    'game' => $gameServerInfo['game'],
+                    'money' => $money,
+                    'qx' => 0,
+                    'uid' => $user['id'],
+                    'uidname' => $user['name'],
+                    'ptid' => $gameServerInfo['ptid'],
+                    'tid' => $payInfo['id'],
+                    'bank' => $payInfo['name'],
+                ];
+                model('UserPay')->save($insertData);
+                $url = "http://yylm.265g.com/?tp=recharge&uid=".$uid."&gid=".$gid."&sid=".$sid."&mid=".$mid."&oid=".$oid."&money=".$money."&pid=".$pid."&bank=".$bank."&sign=".$sign;
+                echo $key."<br>";
+        		//die($url);
+        		header('location:'.$url);
+            }else if($gameServerInfo['ptid'] == 2) {
+                $agent_id = $ptInfo['pid'];
+                $game_id = $gameServerInfo['gid'];
+                $serverNo = $gameServerInfo['sid'];
+                $uid = $user['users'];
+                $OrderID = date('Ymdhis',time()).rand(1000,9999);
+                $amount = intval(trim($data['money']));
+                $paytype = $payInfo['pay'];
+                $rel = urlencode("http://".$_SERVER['SERVER_NAME']);  // 星蝶充值参数充值成功跳转地址
+                $time = time();
+                $charname = ''; // 暂时不知道有什么意思
+                $sign = md5($agent_id.$game_id.$serverNo.$uid.$OrderID.$amount.$paytype.$time.$key);
+                $insertData = [
+                    'oid' => $OrderID,
+                    'game' => $gameServerInfo['game'],
+                    'money' => $amount,
+                    'qx' => 0,
+                    'uid' => $user['id'],
+                    'uidname' => $user['name'],
+                    'ptid' => $gameServerInfo['ptid'],
+                    'tid' => $payInfo['id'],
+                    'bank' => $paytype,
+                ];
+                model('UserPay')->save($insertData);
+            }
+        }
+        return $this->fetch('',[
+            'agent_id' => $agent_id,
+            'game_id' => $game_id,
+            'serverNo' => $serverNo,
+            'uid' => $uid,
+            'OrderID' => $OrderID,
+            'amount' => $amount,
+            'paytype' => $paytype,
+            'rel' => $rel,
+            'time' => $time,
+            'charname' => $charname,
+            'sign' => $sign,
+        ]);
+    }
 }
